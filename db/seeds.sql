6d02bab393fda592fe6d48b5131a342c893ee738
Qhub <qdaman90@gmail.com>
2018-09-04 13:45:59 -0500

    adding DB and Config folder

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Goey Goodness", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Classic", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Oink", false);
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
