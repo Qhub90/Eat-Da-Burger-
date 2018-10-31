f9675b9d778b444ebfc29be17d2df1e6427cc99d
Qhub <qdaman90@gmail.com>
2018-10-31 11:56:27 -0500

    Adding heroku

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,20 @@
 
 // Require mysql
 var mysql = require("mysql");
-
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "burgers_db"
 });
+};
 
 // Connect to the database
 connection.connect(function(err) {
